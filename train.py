73b1ccd4e1df4ea30bd694692a12f2f3a00983e3
00schen <00schen@berkeley.edu>
2018-12-02 12:00:09 -0800

    train

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 from pong_player.py import *
 
-env = gym.make('CartPole-v0').unwrapped
+env = PongEnv()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
